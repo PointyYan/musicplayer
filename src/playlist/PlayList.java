a53e48506dcb790e52c6d05f1aa6bd1eac408ae8
Yan <yjq97@outlook.com>
2017-12-04 16:03:32 +0800

    add some new project file

---
+++ src/playlist/PlayList.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package playlist;
+
+import java.util.*;
+import java.io.File;
+import javax.swing.JList;
+import ui.MainView;
+
+
+/**
+ *
+ * @author yjq97
+ */
+public class PlayList {
+    
+    // 保存所有音乐文件
+	private ArrayList<File> allFile = new ArrayList<File>();
+	// 保存名称
+	private ArrayList<String> musicName = new ArrayList<String>();
+        // 记录列表选择的位置 与上面两个list对应
+	private int chooseIndex;
+        
+        
+        
+        public void init(){
+            musicName.clear();
+            allFile.clear();
+            File f = new File("Music");
+            //如果这个文件夹不存在就创建文件夹
+            if (!f.exists()) {
+			f.mkdirs();
+		} else {
+			//得到文件夹里的所有文件
+			File fa[] = f.listFiles();
+			for (int i = 0; i < fa.length; i++) {
+				File fs = fa[i];
+				if (!fs.isDirectory()) {
+					//得到文件名和文件类型后缀
+					String name = fs.getName().substring(0, fs.getName().indexOf("."));
+					String type = fs.getName().substring(fs.getName().indexOf(".") + 1);
+					if (type.toLowerCase().equals("mp3") || type.toLowerCase().equals("wav")) {
+						musicName.add(name);
+						allFile.add(fs);
+					}
+				}
+			}
+
+		}
+        }
+        
+    
+}
